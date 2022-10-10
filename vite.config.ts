903341558c905d3dd275a0218d414aabbae4fb08
Jabbcode <Jabbsontouch@gmail.com>
2022-10-10 19:55:09 -0400

    Adding package vite-plugin-rewrite-all, so that the Vite 2 plugin does not fix the dev server not rewriting the path includes a dot. And be able to receive the token correctly by url

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import * as path from 'path'
+import pluginRewriteAll from 'vite-plugin-rewrite-all'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [pluginRewriteAll(), react()],
 	resolve: {
 		alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
 	},
